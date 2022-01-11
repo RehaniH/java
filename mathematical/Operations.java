f788082af8be3b5107e5bf96100c28175b1ae475
RehaniH <rehani44perera@gmail.com>
2022-01-11 11:31:21 +0530

    types of operations in java

---
+++ mathematical/Operations.java
@@ -0,0 +1,22 @@
+package mathematical;
+
+public class Operations {
+
+    public static void main(String[] args){
+
+        double x = 10;
+        float y = 17.7f;
+        System.out.println(Math.max(x, y));
+        System.out.println(Math.min(x, y));
+        System.out.println(Math.sqrt(x));
+        System.out.println(Math.abs(-10.22));
+
+        //random number between 0.0 (inclusive) and 1.0(exclusive)
+        System.out.println(Math.random());
+
+        int randomNumber = (int)(Math.random() * 101);
+        System.out.println(randomNumber);
+      
+    }
+    
+}
