5339caa47370ac8bc3e719342b7adc876a7e5fd6
RehaniH <rehani44perera@gmail.com>
2022-03-21 07:41:13 +0530

    basics of java language

---
+++ basics/src/basics/ClassPathExample.java
@@ -0,0 +1,10 @@
+package basics;
+
+public class ClassPathExample {
+
+	public static void main(String[] args) {
+		String currentDirectory = System.getProperty("user.dir");
+		System.out.println(currentDirectory);
+	}
+
+}
---
+++ basics/src/basics/Hello.java
@@ -0,0 +1,27 @@
+package basics;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.print("hello");
+		System.out.println("a new line added after code");
+		System.out.print("this is printed on next line");
+		
+		String[] stringargs = new String[5];
+	//	main(stringargs); recursive -> runtime exception
+		main();
+		main(30);
+//		System.out.println();
+//		System.out.println();
+
+	}
+	
+	public static void main() {
+		System.out.println("Overloaded main method 1");	
+	}
+	
+	public static void main(int a) {
+		System.out.println("Overloaded main method 2 :" + a );	
+	}
+
+}
