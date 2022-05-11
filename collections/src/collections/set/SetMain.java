04c47981e3299b77d5272a585bb3189044355157
himashaperera <rehani.himasha@gmail.com>
2022-05-11 11:11:16 +0530

    added setters and getters

--- collections/src/collections/set/SetMain.java
+++ collections/src/collections/set/SetMain.java
@@ -54,6 +54,30 @@ class Employee{
 		this.ename = ename;
 		this.salary = salary;
 	}
+
+	public int getEmpNo() {
+		return empNo;
+	}
+
+	public void setEmpNo(int empNo) {
+		this.empNo = empNo;
+	}
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
 	
 	
 }
